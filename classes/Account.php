beb43456229913c76ccaa6980332327ed61fab5b
Shawn Webb <lattera@gmail.com>
2012-06-08 16:54:05 -0600

    Add ability to persist accounts. Add basic page for editing existing accounts.

--- classes/Account.php
+++ classes/Account.php
@@ -16,7 +16,9 @@ class Account {
 
     function __construct() {
         $this->dirty = FALSE;
+        $this->account_id = 0;
         $this->notes = array();
+
         $this->keyid =
             $this->username =
             $this->password =
@@ -40,6 +42,62 @@ class Account {
             $this->$name = $value;
     }
 
+    public function Persist() {
+        if ($this->account_id == 0)
+            return $this->Create();
+        else if ($this->dirty)
+            return $this->Update();
+
+        return TRUE;
+    }
+
+    public function Delete() {
+        foreach ($this->notes as $note)
+            $note->Delete();
+
+        db_delete('passdb_accounts')
+            ->condition('account_id', $this->account_id)
+            ->execute();
+    }
+
+    private function Create() {
+        $timestamp = time();
+
+        db_insert('passdb_accounts')
+            ->fields(array(
+                'uid' => $this->uid,
+                'create_timestamp' => $timestamp,
+                'update_timestamp' => $timestamp,
+                'site' => $this->site,
+                'username' => $this->username,
+                'password' => $this->password,
+                'description' => $this->description,
+            ))
+            ->execute();
+
+        $this->create_timestamp = $this->update_timestamp = $timestamp;
+        $this->dirty = FALSE;
+        return TRUE;
+    }
+
+    private function Update() {
+        $this->update_timestamp = time();
+
+        db_update('passdb_accounts')
+            ->fields(array(
+                'update_timestamp' => $this->update_timestamp,
+                'site' => $this->site,
+                'username' => $this->username,
+                'password' => $this->password,
+                'description' => $this->description,
+            ))
+            ->condition('account_id', $this->account_id)
+            ->execute();
+
+        $this->dirty = FALSE;
+        return TRUE;
+    }
+
     public static function LoadAll($uid=0) {
         if ($uid == 0)
             return NULL;
@@ -54,6 +112,19 @@ class Account {
             $accounts[] = Account::LoadFromRecord($record);
 
         return $accounts;
+    }
+
+    public static function Load($account_id) {
+        $record = db_select('passdb_accounts', 'a')
+            ->fields('a')
+            ->condition('account_id', $account_id, '=')
+            ->execute()
+            ->fetchAssoc();
+
+        if ($record !== FALSE)
+            return Account::LoadFromRecord($record);
+
+        return NULL;
     }
 
     protected static function LoadFromRecord($record) {
