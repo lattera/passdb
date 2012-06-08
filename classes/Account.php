fe81c3273837c9e7f2fd299a645ddbc934437f52
Shawn Webb <lattera@gmail.com>
2012-06-08 15:33:33 -0600

    Add code to show the records

--- classes/Account.php
+++ classes/Account.php
@@ -1,16 +1,74 @@
 <?php
 
 class Account {
-    private $uid;
-    private $keyid;
-    private $title;
-    private $username;
-    private $password;
-    private $site;
-    private $description;
-    private $notes;
+    protected $uid;
+    protected $account_id;
+    protected $keyid;
+    protected $title;
+    protected $username;
+    protected $password;
+    protected $site;
+    protected $description;
+    protected $notes;
+    protected $create_timestamp;
+    protected $update_timestamp;
+    protected $dirty;
 
+    function __construct() {
+        $this->dirty = FALSE;
+        $this->notes = array();
+        $this->keyid =
+            $this->username =
+            $this->password =
+            $this->site =
+            $this->description = "";
+    }
 
+    public function __get($name) {
+        if ($name == "password") {
+            /* TODO - Decrypt first */
+            return $this->password;
+        }
+
+        return $this->$name;
+    }
+
+    public function __set($name, $value) {
+        if ($name == "password")
+            $this->password = $value;
+        else
+            $this->$name = $value;
+    }
+
+    public static function LoadAll($uid=0) {
+        if ($uid == 0)
+            return NULL;
+
+        $accounts = array();
+
+        $records = db_select('passdb_accounts', 'a')
+            ->fields('a')
+            ->execute();
+
+        while (($record = $records->fetchAssoc()))
+            $accounts[] = Account::LoadFromRecord($record);
+
+        return $accounts;
+    }
+
+    protected static function LoadFromRecord($record) {
+        $account = new Account;
+        $account->account_id = $record['account_id'];
+        $account->uid = $record['uid'];
+        $account->username = $record['username'];
+        $account->password = $record['password'];
+        $account->site = $record['site'];
+        $account->description = $record['description'];
+        $account->create_timestamp = $record['create_timestamp'];
+        $account->update_timestamp = $record['update_timestamp'];
+
+        return $account;
+    }
 }
 
 ?>
