66a83bef964114aaff39295cf88f2ca3ed2b3381
Shawn Webb <lattera@gmail.com>
2012-06-08 13:08:38 -0600

    Add basic code

---
+++ classes/Account.php
@@ -0,0 +1,16 @@
+<?php
+
+class Account {
+    private $uid;
+    private $keyid;
+    private $title;
+    private $username;
+    private $password;
+    private $site;
+    private $description;
+    private $notes;
+
+
+}
+
+?>
